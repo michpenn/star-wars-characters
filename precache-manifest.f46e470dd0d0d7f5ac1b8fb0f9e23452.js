709d4407c0a3b87a385224ec8b156ea9af350284
michpenn <michpenn91@gmail.com>
2019-01-12 15:34:20 -0500

    Updates

---
+++ precache-manifest.f46e470dd0d0d7f5ac1b8fb0f9e23452.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0127833b3b2c59794ee",
+    "url": "/star-wars-characters/static/js/runtime~main.e0127833.js"
+  },
+  {
+    "revision": "45142f6ce35b29593ca4",
+    "url": "/star-wars-characters/static/js/main.45142f6c.chunk.js"
+  },
+  {
+    "revision": "62d99d70b4c790898ae3",
+    "url": "/star-wars-characters/static/js/1.62d99d70.chunk.js"
+  },
+  {
+    "revision": "45142f6ce35b29593ca4",
+    "url": "/star-wars-characters/static/css/main.82233df2.chunk.css"
+  },
+  {
+    "revision": "62d99d70b4c790898ae3",
+    "url": "/star-wars-characters/static/css/1.8170a9d7.chunk.css"
+  },
+  {
+    "revision": "25a387410b3258884dd76a36218f005b",
+    "url": "/star-wars-characters/index.html"
+  }
+];
